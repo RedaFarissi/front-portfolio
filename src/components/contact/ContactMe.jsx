8511ac279eb136180d80a73c17d181e75116fcd9
RedaFarissi <redaesskouni@gmail.com>
2023-11-09 19:02:36 +0100

    .

--- src/components/contact/ContactMe.jsx
+++ src/components/contact/ContactMe.jsx
@@ -20,7 +20,9 @@ export default function ContactMe(props){
         props.observerAnimation("animation-to-top" , document.querySelectorAll(".animation-direction-to-top") )    
         props.observerAnimation("animation-to-right-1s" , document.querySelectorAll(".animation-direction-to-right-1s") )
         props.observerAnimation("animation-to-right-2s" , document.querySelectorAll(".animation-direction-to-right-2s") )
-        
+        props.observerAnimation("animation-to-right-3s" , document.querySelectorAll(".animation-direction-to-right-3s") )
+        props.observerAnimation("animation-to-right-4s" , document.querySelectorAll(".animation-direction-to-right-4s") )
+        props.observerAnimation("animation-to-right-5s" , document.querySelectorAll(".animation-direction-to-right-5s") )
         props.observerAnimation("animation-to-left-1s" , document.querySelectorAll(".animation-direction-to-left-1s") )
         props.observerAnimation("animation-to-left-2s" , document.querySelectorAll(".animation-direction-to-left-2s") )
         props.observerAnimation("animation-to-left-3s" , document.querySelectorAll(".animation-direction-to-left-3s") )
@@ -29,7 +31,7 @@ export default function ContactMe(props){
 
     return(  
     <main onClick={props.asideHidden} className="contact-main">
-        <div className="container">
+        <div className="contact-map">
 
             <div className="adress row">
                 <div className="adress-box col-md-6">
@@ -38,6 +40,7 @@ export default function ContactMe(props){
                 <SvgMail />
             </div>
         </div>
+
         <div className="contact-msg  m-auto position-relative rounded mt-5 mb-5 pt-0" >
             <h1 className="animation-direction-to-left-1s">CONTACT ME</h1>
             <hr id="hr-bottom-contact"/>
@@ -45,25 +48,25 @@ export default function ContactMe(props){
             <form  method="POST">
                 <input type='text' name='subject' className="subject rounded animation-direction-to-left-2s" placeholder='Title of the email (subject)' required /><br/>
                 <input type='email' name='email' className="rounded animation-direction-to-left-3s" placeholder='Your email' required/><br/>
-                <textarea name='message' className='textarea animation-direction-to-left-4s'> Your message </textarea>
+                <textarea name='message' className='textarea animation-direction-to-left-4s' placeholder='Your message'> </textarea>
                 <button type='submit' className='button btn px-5 py-2 fs-5 animation-direction-to-top'> Send </button>
             </form>
             <div className="contact_box">
-                <h5 id="collection" className='my-3 ms-3'>social</h5>
-                <div className='d-flex'>
+                <h5 id="collection" className='my-3 ms-3 animation-direction-to-right-1s'>social</h5>
+                <div className='d-flex animation-direction-to-right-2s'>
                     <i className="fa-regular fa-location-dot mt-1 mx-3"></i>
                     <p>DR JDID, Rue 55 No 23 Sidi Slimane</p>
                 </div>
 
-                <div className='d-flex'>
+                <div className='d-flex animation-direction-to-right-3s'>
                     <i className="fa-solid fa-phone mt-1 mx-3"></i>
                     <p> +212 6 06 70 56 46 </p>
                 </div>
-                <div className='d-flex'>
+                <div className='d-flex animation-direction-to-right-4s'>
                     <i className="fa-solid fa-envelope mt-1 mx-3"></i>
                     <p> redaesskouni@gmail.com </p>
                 </div>
-                <ul>
+                <ul className="animation-direction-to-right-5s">
                     <li>
                         <a className="btn-github" href="https://github.com/RedaFarissi/"> 
                             <span><i className="fab fa-github text-light"></i></span>
