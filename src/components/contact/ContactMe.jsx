4b903c99ba35065a1baca70c8afcfe98b83e47bf
RedaFarissi <redaesskouni@gmail.com>
2023-11-09 17:02:31 +0100

    Animation caontact me

--- src/components/contact/ContactMe.jsx
+++ src/components/contact/ContactMe.jsx
@@ -20,7 +20,11 @@ export default function ContactMe(props){
         props.observerAnimation("animation-to-top" , document.querySelectorAll(".animation-direction-to-top") )    
         props.observerAnimation("animation-to-right-1s" , document.querySelectorAll(".animation-direction-to-right-1s") )
         props.observerAnimation("animation-to-right-2s" , document.querySelectorAll(".animation-direction-to-right-2s") )
+        
         props.observerAnimation("animation-to-left-1s" , document.querySelectorAll(".animation-direction-to-left-1s") )
+        props.observerAnimation("animation-to-left-2s" , document.querySelectorAll(".animation-direction-to-left-2s") )
+        props.observerAnimation("animation-to-left-3s" , document.querySelectorAll(".animation-direction-to-left-3s") )
+        props.observerAnimation("animation-to-left-4s" , document.querySelectorAll(".animation-direction-to-left-4s") )
     }, []);
 
     return(  
@@ -35,14 +39,14 @@ export default function ContactMe(props){
             </div>
         </div>
         <div className="contact-msg  m-auto position-relative rounded mt-5 mb-5 pt-0" >
-            <h1>CONTACT ME</h1>
+            <h1 className="animation-direction-to-left-1s">CONTACT ME</h1>
             <hr id="hr-bottom-contact"/>
-            <p className="line">I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you to create modern site web</p>
+            <p className="line animation-to-right-2s">I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you to create modern site web</p>
             <form  method="POST">
-                <input type='text' name='subject' className="subject rounded" placeholder='Title of the email (subject)' required /><br/>
-                <input type='email' name='email' className="rounded" placeholder='Your email' required/><br/>
-                <textarea name='message' className='textarea'> Your message </textarea>
-                <button type='submit' className='button btn px-5 py-2 fs-5'> Send </button>
+                <input type='text' name='subject' className="subject rounded animation-direction-to-left-2s" placeholder='Title of the email (subject)' required /><br/>
+                <input type='email' name='email' className="rounded animation-direction-to-left-3s" placeholder='Your email' required/><br/>
+                <textarea name='message' className='textarea animation-direction-to-left-4s'> Your message </textarea>
+                <button type='submit' className='button btn px-5 py-2 fs-5 animation-direction-to-top'> Send </button>
             </form>
             <div className="contact_box">
                 <h5 id="collection" className='my-3 ms-3'>social</h5>
@@ -83,8 +87,6 @@ export default function ContactMe(props){
                 </ul>
             </div>
         </div>
-
-        
     </main>
     )
 }
